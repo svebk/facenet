02d7ce53a47ec1d7cd491d2bde6d5aba40fe3cff
Svebor Karaman <svebor.karaman@gmail.com>
2019-03-25 19:04:54 -0400

    change align import

--- contributed/face.py
+++ contributed/face.py
@@ -40,6 +40,7 @@ from scipy import misc
 #import align.detect_face
 import facenet
 from facenet.src import align
+from facenet.src.align import detect_face
 
 gpu_memory_fraction = 0.3
 facenet_model_checkpoint = os.path.dirname(__file__) + "/../model_checkpoints/20170512-110547"
